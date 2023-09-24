32bfa828eb31ae80151fa808105cffa8c1496214
ChaselWating <1368959273@qq.com>
2023-09-24 08:30:51 +0800

    day230924up

--- LeetCode/solution_23_09_21/problem_24/Solution.java
+++ LeetCode/solution_23_09_21/problem_24/Solution.java
@@ -1,14 +1,15 @@
-class ListNode{
-	int val;
-	ListNode next;
-    ListNode(){}
-	ListNode(int val){
-		this.val = val;
-	}
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+	ListNode(int val, ListNode next) { this.val = val; this.next = next; }
 }
 
 public class Solution {
-	public ListNode swapPairs(ListNode head) {
+	
+	//利用双指针，但好像不太对
+	public ListNode swapPairs0(ListNode head) {
 		ListNode prev = head;
 		ListNode cur = head.next;
 		while(cur != null || prev != null) {
@@ -18,4 +19,20 @@ public class Solution {
 			cur = prev.next;
 		}
     }
+	
+	//利用虚拟头节点，用一个指针解决问题
+	public ListNode swapPairs(ListNode head) {
+		ListNode dummyhead = new ListNode(0, head);
+		ListNode cur = dummyhead;
+		while(cur.next != null && cur.next.next != null) {
+			ListNode temp  = cur.next;
+			ListNode temp1 = cur.next.next.next;
+			cur.next = cur.next.next;
+			cur.next.next = temp;
+			temp.next = temp1;
+			cur = temp;
+		}
+		return dummyhead.next;
+	}
+	
 }
